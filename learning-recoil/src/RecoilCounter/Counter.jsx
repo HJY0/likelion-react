6a891279a211abd00fbb11bc2c54bba6cd6ff4f4
Jayoung Hong <gripix324@gmail.com>
2023-03-07 15:38:29 +0900

    Recoil 훅의 유형별 사용법

--- learning-recoil/src/RecoilCounter/Counter.jsx
+++ learning-recoil/src/RecoilCounter/Counter.jsx
@@ -2,18 +2,15 @@ import classes from "./Counter.module.css";
 import CountButton from "./CountButton";
 import Count from "./Count";
 
-import { useRecoilState } from "recoil";
-import { countState } from "@/@store/countState";
-
 export default function Counter() {
-  const [count, setCount] = useRecoilState(countState);
+  // 읽기(GET)/쓰기(SET)  useRecoilState
+  // 읽기(GET)            useRecoilValue
+  // 쓰기(SET)            useSetRecoilState
 
   return (
     <div className={classes.ReactCounter}>
-      <Count>{count}</Count>
-      <CountButton onClick={() => setCount((count) => count + 1)}>
-        리코일 카운트
-      </CountButton>
+      <Count />
+      <CountButton>리코일 카운트</CountButton>
     </div>
   );
 }
