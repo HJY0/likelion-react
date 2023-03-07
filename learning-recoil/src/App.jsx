10537560bc2ba472eb4b5630c1065bb990187462
Jayoung Hong <gripix324@gmail.com>
2023-03-07 15:21:17 +0900

    ReactCount, RecoilCount.jsx 생성

--- learning-recoil/src/App.jsx
+++ learning-recoil/src/App.jsx
@@ -1,21 +1,13 @@
 import "./App.css";
-import { useRecoilState } from "recoil";
-import { countState } from "./@store/countState";
-import { useState } from "react";
+import ReactCounter from "@/ReactCounter/Counter";
+import RecoilCounter from "@/RecoilCounter/Counter";
 
 function App() {
-  const [count, setCount] = useRecoilState(countState);
-  const [reactCount, setReactCount] = useState(0);
-
   return (
     <div className="App">
-      <button onClick={() => setCount((count) => count + 1)}>
-        리코일 카운트 = {count}
-      </button>
+      <RecoilCounter />
 
-      <button onClick={() => setReactCount((count) => count + 1)}>
-        리액트 카운트 = {reactCount}
-      </button>
+      <ReactCounter />
     </div>
   );
 }
